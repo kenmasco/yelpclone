bd99a6c6667b50686e5b607d035df6bbf51cf494
Kenndy <kennerdeigh@me.com>
2014-01-13 16:10:04 +0000

    Added creating new restaurants feauture

---
+++ spec/features/create_restaurants_feature_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "creating restuarants" do
+  
+  context 'creating a name and description' do
+  
+    it 'should display the new restaurant on the list of restaurants' do
+      visit '/restaurants/new'
+      fill_in 'Name', with: 'McDonalds'
+      fill_in 'Description', with: "I'm lovin' it"
+      click_button 'Create Restaurant'
+
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content 'McDonalds'
+    end
+
+  end
+
+end
